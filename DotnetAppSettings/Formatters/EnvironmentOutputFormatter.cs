6bb27d4f31d8273f7b4bb84fe7b7cb81327e5efb
Tho Ho <code@tho.email>
2021-09-15 10:34:41 +0800

    Format code

--- DotnetAppSettings/Formatters/EnvironmentOutputFormatter.cs
+++ DotnetAppSettings/Formatters/EnvironmentOutputFormatter.cs
@@ -10,7 +10,7 @@ namespace DotnetAppSettings.Formatters
         public async Task WriteAsync(Stream stream, IEnumerable<AzureAppSetting> settings)
         {
             var content = settings
-                .Select(s => $"- {s.Name}={ s.Value}");
+                .Select(s => $"- {s.Name}={s.Value}");
 
             using var writer = new StreamWriter(stream, leaveOpen: true);
 
