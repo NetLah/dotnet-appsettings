c82d53bcf260b588150a79a9117fd7c468636858
Tho Ho <code@tho.email>
2021-11-10 22:17:15 +0800

    Add process execution test

---
+++ DotnetAppSettings.Test/AppSettingConversionTest.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotnetAppSettings.Test
+{
+    public class AppSettingConversionTest
+    {
+        private const string FullAzure = @"[
+  {
+    ""name"": ""AllowedHosts"",
+    ""value"": ""*"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Array__0__Name"",
+    ""value"": ""Value1"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Array__1__Name"",
+    ""value"": ""Value2"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""ConnectionStrings__DefaultConnection"",
+    ""value"": ""Data Source=localhost;Initial Catalog=database-73628d830a33;Integrated Security=True;MultipleActiveResultSets=true;"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Default"",
+    ""value"": ""Debug"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Microsoft"",
+    ""value"": ""Warning"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Microsoft.Hosting.Lifetime"",
+    ""value"": ""Information"",
+    ""slotSetting"": false
+  }
+]";
+
+        private static Task<string[]> AppSettingsExecuteAsync(string parameters) => SimpleExecuteHelper.ExecuteAsync("DotnetAppSettings", parameters);
+
+        [Fact]
+        public async Task Convert_Azure_Main()
+        {
+            var result = await AppSettingsExecuteAsync("Files/appsettings.json");
+
+            Assert.Equal(@"[
+  {
+    ""name"": ""AllowedHosts"",
+    ""value"": ""*"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Array__0__Name"",
+    ""value"": ""Value1"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Array__1__Name"",
+    ""value"": ""Value2"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Default"",
+    ""value"": ""Information"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Microsoft"",
+    ""value"": ""Warning"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Microsoft.Hosting.Lifetime"",
+    ""value"": ""Information"",
+    ""slotSetting"": false
+  }
+]", string.Join(Environment.NewLine, result));
+        }
+
+        [Fact]
+        public async Task Convert_Azure_MainDev()
+        {
+            var result = await AppSettingsExecuteAsync("Files/appsettings.json Files/appsettings.Development.json");
+
+            Assert.Equal(FullAzure, string.Join(Environment.NewLine, result));
+        }
+
+        [Fact]
+        public async Task Convert_Azure_DevMain()
+        {
+            var result = await AppSettingsExecuteAsync("Files/appsettings.Development.json Files/appsettings.json");
+            Assert.Equal(@"[
+  {
+    ""name"": ""AllowedHosts"",
+    ""value"": ""*"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Array__0__Name"",
+    ""value"": ""Value1"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Array__1__Name"",
+    ""value"": ""Value2"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""ConnectionStrings__DefaultConnection"",
+    ""value"": ""Data Source=localhost;Initial Catalog=database-73628d830a33;Integrated Security=True;MultipleActiveResultSets=true;"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Default"",
+    ""value"": ""Information"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Microsoft"",
+    ""value"": ""Warning"",
+    ""slotSetting"": false
+  },
+  {
+    ""name"": ""Logging__LogLevel__Microsoft.Hosting.Lifetime"",
+    ""value"": ""Information"",
+    ""slotSetting"": false
+  }
+]", string.Join(Environment.NewLine, result));
+        }
+
+        [Fact]
+        public async Task Convert_Azure_Path()
+        {
+            var result = await AppSettingsExecuteAsync("--path Files appsettings.json appsettings.Development.json");
+            Assert.Equal(FullAzure, string.Join(Environment.NewLine, result));
+        }
+
+        [Fact]
+        public async Task Convert_DockerCompose_Environemnt()
+        {
+            var result = await AppSettingsExecuteAsync("Files/appsettings.json Files/appsettings.Development.json --environment");
+            Assert.Equal(@"- AllowedHosts=*
+- Array__0__Name=Value1
+- Array__1__Name=Value2
+- ConnectionStrings__DefaultConnection=Data Source=localhost;Initial Catalog=database-73628d830a33;Integrated Security=True;MultipleActiveResultSets=true;
+- Logging__LogLevel__Default=Debug
+- Logging__LogLevel__Microsoft=Warning
+- Logging__LogLevel__Microsoft.Hosting.Lifetime=Information
+", string.Join(Environment.NewLine, result));
+        }
+
+        [Fact]
+        public async Task Convert_Text()
+        {
+            var result = await AppSettingsExecuteAsync("Files/appsettings.json Files/appsettings.Development.json --text");
+            Assert.Equal(@"AllowedHosts
+*
+
+Array__0__Name
+Value1
+
+Array__1__Name
+Value2
+
+ConnectionStrings__DefaultConnection
+Data Source=localhost;Initial Catalog=database-73628d830a33;Integrated Security=True;MultipleActiveResultSets=true;
+
+Logging__LogLevel__Default
+Debug
+
+Logging__LogLevel__Microsoft
+Warning
+
+Logging__LogLevel__Microsoft.Hosting.Lifetime
+Information
+", string.Join(Environment.NewLine, result));
+        }
+    }
+}
---
+++ DotnetAppSettings.Test/SimpleExecuteHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetAppSettings.Test
+{
+    internal static class SimpleExecuteHelper
+    {
+        public static async Task<string[]> ExecuteAsync(string executable, string arguments)
+        {
+            var output = new List<string>();
+
+            using var process = new Process();
+            process.StartInfo.FileName = executable;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.EnableRaisingEvents = true;
+
+            process.ErrorDataReceived += (o, e) => { if (e.Data is { } data) { output.Add(data); } };
+            process.OutputDataReceived += (o, e) => { if (e.Data is { } data) { output.Add(data); } };
+
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(15));
+
+            await process.WaitForExitAsync(cts.Token);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));      // wait 1s for output result firing on slow computer
+
+            return output.ToArray();
+        }
+    }
+
+#if NETCOREAPP3_1
+    internal static class ProcessExtensions
+    {
+        public static Task WaitForExitAsync(this Process process, CancellationToken cancellationToken = default)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            using var registration = cancellationToken.Register(() => tcs.TrySetCanceled());
+
+            _ = Task.Run(() =>
+            {
+                Exception captureException = null;
+                try
+                {
+                    process.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    captureException = ex;
+                }
+
+                if (captureException != null)
+                {
+                    tcs.TrySetException(captureException);
+                }
+                else
+                {
+                    tcs.TrySetResult(process.HasExited);
+                }
+            });
+
+            return tcs.Task;
+        }
+    }
+#endif
+}
